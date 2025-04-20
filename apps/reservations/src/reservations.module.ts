52668cf66ea04e1a05238de8f680f935c0fa580b
soyalper <soy.m.alper@gmail.com>
2025-04-20 10:57:50 +0300

    Passport module
    Add passport module

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -5,9 +5,18 @@ import { DatabaseModule, HttpExceptionFilter, LoggerModule } from '@app/common';
 import { ReservationsRepository } from './reservations.repository';
 import { Reservation, ReservationSchema } from './models/reservation.schema';
 import { APP_FILTER } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+    }),
     DatabaseModule,
     DatabaseModule.forFeature([
       { name: Reservation.name, schema: ReservationSchema },
